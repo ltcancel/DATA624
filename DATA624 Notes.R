b7670f1109403536754412f6a23b3e19a13152f1
ltcancel <ltcancel@gmail.com>
2022-06-03 17:43:12 -0400

    update

---
+++ DATA624 Notes.R
@@ -0,0 +1,12 @@
+# Time series graphics
+
+#Annual time series starting in 2021
+ts(c(123,39,78,52,110), start=2021)
+
+#Monthly time series starting in 2003
+ts(z, start=2003, frequency=12)
+
+Other frequencies: 1, 7, 4, 12, 52, 60, 1440, 10080, 525960
+If doing time, use minutes
+
+
